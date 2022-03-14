802ab69351b70dd8efd123a959887e7516591fc0
mohamedellithy <develomohamed22@gmail.com>
2022-03-14 20:38:40 +0200

    add l

--- app/DataTables/ForexCompanyDataTable.php
+++ app/DataTables/ForexCompanyDataTable.php
@@ -76,8 +76,12 @@ class ForexCompanyDataTable extends DataTable
                         Button::make('reload')
                     )
                     ->parameters([
-                       'responsive' => true,
-                        'autoWidth' => false
+                        'responsive' => true,
+                        'autoWidth' => false,
+                        'lengthMenu' => [
+                                [ 10, 25, 50, -1 ],
+                                [ '10 rows', '25 rows', '50 rows', 'Show all' ]
+                        ]
                     ]);
     }
 
