3628600aed0a42b70eade0b826d85253454fda7e
mohamedellithy <develomohamed22@gmail.com>
2022-03-16 00:37:14 +0200

    yrwe

--- app/DataTables/AccountsDataTable.php
+++ app/DataTables/AccountsDataTable.php
@@ -65,7 +65,7 @@ class AccountsDataTable extends DataTable
         $accounts_Query = Account::select('*');
         
         if($this->from){
-            $accounts_Query = $accounts_Query->where('created_at',$this->from)->get();
+            $accounts_Query = $accounts_Query->where('created_at','2022-03-05')->get();
         }
 
         //$accounts_Query = $accounts_Query->get();
