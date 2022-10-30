3cf083e546c93cf2e8c5125a4e46b6221e16f2e0
mohamedellithy <develomohamed22@gmail.com>
2022-10-30 20:36:24 +0200

    pl

--- resources/views/admin/cashback-account/index.blade.php
+++ resources/views/admin/cashback-account/index.blade.php
@@ -63,7 +63,7 @@
                 </div>
                 <div class="form-group data-search">
                     <br/>
-                    <button type="button" class="btn btn-danger btn-sm btn_delete_selected" style="margin: 18px -11px 0px 93px;width: 100%;padding: 8px;">حذف المحدد</button>
+                    <button type="button" class="btn btn-danger btn-sm btn_delete_selected" style="margin: 18px -11px 0px 49px;width: 100%;padding: 8px 0px;">حذف المحدد</button>
                 </div>
             </div>
             {!! $dataTable->table() !!}
