bbc98ce420af28f0f6267597969ef507ce0197c2
mohamedellithy <develomohamed22@gmail.com>
2022-10-31 12:05:30 +0200

    kl

--- database/migrations/2022_10_31_111459_create_expire_cashbacks_table.php
+++ database/migrations/2022_10_31_111459_create_expire_cashbacks_table.php
@@ -17,7 +17,6 @@ class CreateExpireCashbacksTable extends Migration
             $table->engine = 'InnoDB';
             $table->id();
             $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->decimal('value', 10, 2)->required();
             $table->timestamps();
         });
