e28b677807974f458f4c10d5e4a0aa1fe8e06e74
mohamedellithy <develomohamed22@gmail.com>
2022-10-31 13:59:59 +0200

    qa

--- app/Http/Controllers/ExpireCashbacksController.php
+++ app/Http/Controllers/ExpireCashbacksController.php
@@ -13,7 +13,7 @@ class ExpireCashbacksController extends Controller
         $users = User::where('role','!=',1)->get();
         $items = [];
         foreach($users as $user):
-            $items []= $user->total_cashback_can_withdraw();
+            $items []= $user->cashbacks()->sum('value'); //$user->total_cashback_can_withdraw();
         endforeach;
         dd($items);
     }
