9d34f9d38057a231ce4ad3efc3326ca41266f8be
mohamedellithy <develomohamed22@gmail.com>
2022-11-01 01:02:55 +0200

    wq

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\User;
 use App\ExpireCashback;
+use Illuminate\Support\Facades\Http;
 class Kernel extends ConsoleKernel
 {
     /**
@@ -27,19 +28,20 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->call(function(){
-            $date_max_ended = strtotime("-".Options()->setting['max_date_cashback_withdraw']." months");
-            $users = User::where('role','!=',1)->get();
-            foreach($users as $user):
-                $rest_value = $user->total_cashback_can_withdraw() - $user->cashbacks()->where('cash_backs.created_at','>=',date('Y-m-d H:i:s',$date_max_ended))->sum('value');
-                if($rest_value > 0):
-                    $user->expire_cashbacks()->updateOrCreate([
-                        "user_id" => $user->id
-                        ],[
-                        'value'   => $rest_value
-                    ]);
-                endif;
-            endforeach;
-        })->name('run_expire_cashbacks')->withoutOverlapping()->at('01:00')->timezone('Africa/Cairo')->onOneServer();
+            // $date_max_ended = strtotime("-".Options()->setting['max_date_cashback_withdraw']." months");
+            // $users = User::where('role','!=',1)->get();
+            // foreach($users as $user):
+            //     $rest_value = $user->total_cashback_can_withdraw() - $user->cashbacks()->where('cash_backs.created_at','>=',date('Y-m-d H:i:s',$date_max_ended))->sum('value');
+            //     if($rest_value > 0):
+            //         $user->expire_cashbacks()->updateOrCreate([
+            //             "user_id" => $user->id
+            //             ],[
+            //             'value'   => $rest_value
+            //         ]);
+            //     endif;
+            // endforeach;
+            Http::get("https://webhook.site/b2156df9-6b90-4eea-a8fd-c10e9b29acb8");
+        })->name('run_expire_cashbacks')->withoutOverlapping()->everyMinute()->timezone('Africa/Cairo')->onOneServer();
 
         //->monthly();
     }
