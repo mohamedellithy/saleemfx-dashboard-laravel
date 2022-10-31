a05dd72a26b79a2b1dbce6db1eda4a4d3997da75
mohamedellithy <develomohamed22@gmail.com>
2022-10-31 13:45:14 +0200

    qaz

--- app/Http/Controllers/ExpireCashbacksController.php
+++ app/Http/Controllers/ExpireCashbacksController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Account;
+use App\User;
 class ExpireCashbacksController extends Controller
 {
     //
 
     public function index(){
-        echo "hi mohamed";
+        $users = User::where('role',1)->get();
+        dd($users);
     }
 }
