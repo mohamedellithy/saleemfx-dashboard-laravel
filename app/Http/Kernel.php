df90df3b66bf7060cc820631f84722da7a6d479e
mohamedellithy <develomohamed22@gmail.com>
2022-11-01 16:36:49 +0200

    middlewate fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \App\Http\Middleware\CheckIfHaveReferenceAffiliate::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\LanguageSwitcher::class,
     ];
 
     /**
